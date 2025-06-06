398f7b9d1f9689f69d88576174ce06e2c2d8dc1f
JoaquinFerman <joacoferman@gmail.com>
2025-06-06 11:35:48 -0300

    conflicts resolved

--- Parcial/backend/routes/ventas.js
+++ Parcial/backend/routes/ventas.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../services/db')
 const ventasController = require('../controllers/ventas.controller');
 
 router.get('/', (req, res) => {
