fd9c3ff4b320621e3f6ebc6fc547c59134352c85
JoaquinFerman <joacoferman@gmail.com>
2025-06-25 16:20:31 -0300

    Ventas sql corrected, precio_total added to Ventas

--- Parcial/backend/database/addVentas.sql
+++ Parcial/backend/database/addVentas.sql
@@ -1,3 +1,3 @@
-INSERT INTO Ventas (fecha, cliente_nombre) VALUES
-('2025-06-19', 'Matias', ''),
-('2025-06-20', 'Mateo');
+INSERT INTO Ventas (fecha, cliente_nombre, precio_total) VALUES
+('2025-06-19', 'Matias', '120000'),
+('2025-06-20', 'Mateo', '160000');
