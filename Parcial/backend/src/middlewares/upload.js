b12260e0e5c295f0d2041dbf01aaba7ce2e9c358
JoaquinFerman <joacoferman@gmail.com>
2025-07-09 20:12:18 -0300

    multer fix

--- Parcial/backend/src/middlewares/upload.js
+++ Parcial/backend/src/middlewares/upload.js
@@ -1,11 +1,12 @@
 const multer = require('multer');
 const path = require('path');
 
-const imagePath = path.join(__dirname, '..', 'Public', 'images');
+const imagePath = path.join(__dirname, '..', '..', 'Public', 'images');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, imagePath),
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const { name } = req.body
+    cb(null, name + '.webp');
 }
 });
 
